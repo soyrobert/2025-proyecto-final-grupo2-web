7e35ba0a5cea9eae2dde8d72d6fb9e4edeb0c459
Robert Castro <ra.castro2@uniandes.edu.co>
2025-04-06 23:42:21 -0500

    Url de registro de usuarios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,4 +6,5 @@ export const environment = {
     production: false,
     apiBaseUrl: 'http://34.49.18.136/api/v1/seguridad/gestor_usuarios/r',
     proveedoresApiUrl: 'http://34.49.18.136/api/v1/inventario/gestor_proveedores',
+    usuariosApiUrl: 'http://34.49.18.136/api/v1/seguridad/gestor_usuarios/w',
 };
