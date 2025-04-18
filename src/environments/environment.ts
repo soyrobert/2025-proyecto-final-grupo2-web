b31469716178eba60697797635b7ab3f6fdc4747
Robert Castro <ra.castro2@uniandes.edu.co>
2025-04-18 18:00:58 -0500

    Servicio de obtener productos con filtros

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,5 +8,6 @@ export const environment = {
     proveedoresApiUrl: 'http://localhost:3003/api/v1/inventario/gestor_proveedores',
     usuariosApiUrl: 'http://localhost:3011/api/v1/seguridad/gestor_usuarios/w',
     productosApiUrl: 'http://localhost:3001/api/v1/inventario/gestor_productos',
+    busquedaProductosApiUrl: 'http://localhost:3001/api/v2/inventario/gestor_productos',
     storageSignedUrlEndpoint: 'https://service-signed-urls-224260405910.us-central1.run.app'
 };
