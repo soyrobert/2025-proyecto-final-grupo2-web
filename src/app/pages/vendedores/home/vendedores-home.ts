2192db749f9a59828851a3fa5e97d8eb5d34eaa6
Robert Castro <ra.castro2@uniandes.edu.co>
2025-05-15 11:07:26 -0500

    feat(vendedores): implement vendedores home page layout and structure

--- src/app/pages/vendedores/home/vendedores-home.ts
+++ src/app/pages/vendedores/home/vendedores-home.ts
@@ -4,12 +4,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   standalone: true,
   selector: 'app-vendedores-home',
-  template: `
-    <div class="p-4">
-      <h1 class="text-xl font-bold">Home de Vendedores</h1>
-      <p></p>
-    </div>
-  `,
+  templateUrl: './vendedores-home.html',
   imports: [CommonModule],
 })
 export class VendedoresHome {}
