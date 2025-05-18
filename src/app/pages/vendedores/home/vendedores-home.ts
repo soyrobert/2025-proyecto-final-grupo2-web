3794aac8ca6af93646d4f83e046c0ad2ce03e5c9
Robert Castro <ra.castro2@uniandes.edu.co>
2025-05-18 08:12:00 -0500

    feat(vendedores): add translation support for home page and update language files

--- src/app/pages/vendedores/home/vendedores-home.ts
+++ src/app/pages/vendedores/home/vendedores-home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   standalone: true,
   selector: 'app-vendedores-home',
   templateUrl: './vendedores-home.html',
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
 })
 export class VendedoresHome {}
